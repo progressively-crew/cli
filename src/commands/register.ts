f60bb96865749b4c00047f6cd1f121adf016fe75
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-07-18 20:23:19 +0200

    feat: create the register command

---
+++ src/commands/register.ts
@@ -0,0 +1,51 @@
+import { Args, Command, Flags, ux } from "@oclif/core";
+import { getHttpClient } from "../utils/http";
+
+export default class Register extends Command {
+  static description = "describe the command here";
+
+  static examples = ["<%= config.bin %> <%= command.id %>"];
+
+  static flags = {
+    // flag with a value (-n, --name=VALUE)
+    name: Flags.string({ char: "n", description: "name to print" }),
+    // flag with no value (-f, --force)
+    force: Flags.boolean({ char: "f" }),
+  };
+
+  static args = {
+    file: Args.string({ description: "file to read" }),
+  };
+
+  public async run(): Promise<void> {
+    const httpClient = await getHttpClient();
+
+    const fullname = await ux.prompt("What is your fullname?");
+    const email = await ux.prompt("What is your email?");
+    const password = await ux.prompt("What is your password?");
+
+    try {
+      const response = await httpClient.post("/auth/register", {
+        fullname,
+        email,
+        password,
+      });
+
+      switch (response.status) {
+        case 201: {
+          const { data: user } = response;
+
+          this.log(`${user.fullname}, your account has been created 🎉`);
+          break;
+        }
+
+        default:
+          break;
+      }
+    } catch {
+      this.error(
+        "An error has been detected. Did you already create the admin user?"
+      );
+    }
+  }
+}
