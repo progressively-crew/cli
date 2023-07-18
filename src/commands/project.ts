8ac71324942141b29b88856265a12348746fcd63
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-07-18 23:51:24 +0200

    feat: allow project creation before selection

--- src/commands/project.ts
+++ src/commands/project.ts
@@ -1,5 +1,5 @@
-import { Args, Command, Flags } from "@oclif/core";
-import { select } from "@inquirer/prompts";
+import { Command, Flags, ux } from "@oclif/core";
+import { select, confirm } from "@inquirer/prompts";
 import { updateConfig } from "../utils/config";
 import { getHttpClient } from "../utils/http";
 
@@ -9,22 +9,46 @@ export default class Project extends Command {
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: "f" }),
+    create: Flags.boolean({
+      char: "C",
+      description: "Create a project before selection",
+    }),
   };
 
-  static args = {
-    file: Args.string({ description: "file to read" }),
-  };
+  static args = {};
 
   public async run(): Promise<void> {
-    // const { args, flags } = await this.parse(Project);
+    const { flags } = await this.parse(Project);
 
     const httpClient = await getHttpClient(true);
 
     const { data: projects } = await httpClient.get("/projects");
+
+    if (projects.length === 0 || flags.create) {
+      if (projects.length === 0) {
+        const shouldCreateProject = await confirm({
+          message:
+            "We couldn't find any project, do you want to create one now?",
+        });
+
+        if (shouldCreateProject === false) {
+          this.log(
+            "You skipped project creation, you can still do this later or with web interface"
+          );
+
+          return;
+        }
+      }
+
+      const projectName = await ux.prompt("What is the name of your project?");
+      const httpClient = await getHttpClient(true);
+
+      const { data: newProject } = await httpClient.post("/projects", {
+        name: projectName,
+      });
+
+      projects.push({ project: newProject });
+    }
 
     const choices = projects.map(({ project }: any) => ({
       name: project.name,
