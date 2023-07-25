6e42cebaee3ffab33521b00e65e792673f57e069
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-07-25 19:12:30 +0200

    feat: create the me command

---
+++ src/commands/me.ts
@@ -0,0 +1,25 @@
+import { Command } from "@oclif/core";
+import { getHttpClient } from "../utils/http";
+
+export default class Me extends Command {
+  static description = "describe the command here";
+
+  static examples = ["<%= config.bin %> <%= command.id %>"];
+
+  static flags = {};
+
+  static args = {};
+
+  public async run(): Promise<void> {
+    const httpClient = await getHttpClient(true);
+
+    const { data } = await httpClient.get("/users/me");
+
+    this.log(
+      `
+Full name : ${data.fullname}
+Email     : ${data.email}
+    `.trim()
+    );
+  }
+}
---
+++ test/commands/me.test.ts
@@ -0,0 +1,17 @@
+import {expect, test} from '@oclif/test'
+
+describe('me', () => {
+  test
+  .stdout()
+  .command(['me'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['me', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  })
+})
