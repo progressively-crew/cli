eebae49148a30c99ef6767d1cf0db62e212a3dce
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-07-31 19:34:11 +0200

    it's working

--- src/commands/types.ts
+++ src/commands/types.ts
@@ -1,6 +1,7 @@
 import { Command, Flags } from "@oclif/core";
 import fsPromises from "node:fs/promises";
 import { getHttpClient } from "../utils/http";
+import { readConfig } from "../utils/config";
 
 export default class Types extends Command {
   static description = "Generate types based on your Progressively instance";
@@ -29,18 +30,11 @@ export default class Types extends Command {
     const { flags } = await this.parse(Types);
 
     const httpClient = await getHttpClient(true);
+    const { client_key } = await readConfig();
 
-    // TODO: make token dynamic or remove it
-    const { data } = await httpClient.get(
-      "/sdk/ce81b569-2f81-4e4f-bef4-0896bcf06a2a/types"
-    );
+    const { data } = await httpClient.get(`/sdk/${client_key}/types/gen`);
 
     const targetFilePath = `${flags.output}/${flags.name}`;
-
-    console.log(
-      "🚀 ~ file: types.ts:41 ~ Types ~ run ~ targetFilePath:",
-      targetFilePath
-    );
 
     await fsPromises.writeFile(targetFilePath, data);
 
