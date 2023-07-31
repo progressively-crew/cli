687df2dd678486df437b0a3df0aaa60a6ccaee5d
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-07-31 13:15:22 +0200

    fix: authenticate before making the create flag request

--- src/utils/flag.ts
+++ src/utils/flag.ts
@@ -3,15 +3,19 @@ import { readConfig } from "./config";
 import { getHttpClient } from "./http";
 
 export async function createFlag() {
-  const config = await readConfig();
-  const httpClient = await getHttpClient();
+  const { project_id } = await readConfig();
+  const httpClient = await getHttpClient(true);
 
   const name = await input({ message: "What is the name of the flag" });
   const description = await input({
     message: "What is the description of the flag",
   });
 
-  return httpClient.post(`/projects/${config.project_id}/flags`, {
+  if (!project_id) {
+    throw new Error("Couldn't find the project");
+  }
+
+  return httpClient.post(`/projects/${project_id}/flags`, {
     name,
     description,
   });
