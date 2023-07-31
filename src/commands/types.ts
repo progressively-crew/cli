0441f9f4aa0e7c289d141ac8b0c1073c352f0cc4
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-07-31 13:10:14 +0200

    wip on types generation

--- src/commands/types.ts
+++ src/commands/types.ts
@@ -1,27 +1,46 @@
-import { Command } from "@oclif/core";
+import { Command, Flags } from "@oclif/core";
 import fsPromises from "node:fs/promises";
 import { getHttpClient } from "../utils/http";
 
 export default class Types extends Command {
-  static description = "describe the command here";
+  static description = "Generate types based on your Progressively instance";
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  static flags = {};
+  static flags = {
+    name: Flags.string({
+      required: false,
+      default: "progressively.d.ts",
+      char: "N",
+      description: "Name of the generated types file",
+    }),
+    output: Flags.string({
+      required: false,
+      default: process.cwd(),
+      char: "O",
+      description:
+        "Relative path of the folder where the types file should be generated",
+    }),
+  };
 
   static args = {};
 
   public async run(): Promise<void> {
+    const { flags } = await this.parse(Types);
+
     const httpClient = await getHttpClient(true);
 
     // TODO: make token dynamic or remove it
     const { data } = await httpClient.get(
-      "/sdk/ed2d06f3-cb98-417c-9475-e9eec152310b/types"
+      "/sdk/ce81b569-2f81-4e4f-bef4-0896bcf06a2a/types"
     );
 
-    // TODO: find a name for the generated types file
-    // and add it to gitignore.
-    const targetFilePath = `${process.cwd()}/progressivelyTypes.ts`;
+    const targetFilePath = `${flags.output}/${flags.name}`;
+
+    console.log(
+      "🚀 ~ file: types.ts:41 ~ Types ~ run ~ targetFilePath:",
+      targetFilePath
+    );
 
     await fsPromises.writeFile(targetFilePath, data);
 
