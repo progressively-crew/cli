ae605f40db9a0138f28b4b7b573543d51f5286ea
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-08-13 18:09:52 +0200

    test: skip project creation

--- test/commands/project.test.ts
+++ test/commands/project.test.ts
@@ -1,17 +1,21 @@
 import { expect, test } from "@oclif/test";
+import * as prompts from "@inquirer/prompts";
+// import * as configUtils from "../../src/utils/config";
+// import Sinon from "sinon";
+
+// const configUtilsMock = Sinon.mock(configUtils);
 
 describe("project", () => {
   test
     .stdout()
-    .command(["project"])
-    .it("runs hello", (ctx) => {
-      expect(ctx.stdout).to.contain("hello world");
-    });
-
-  test
-    .stdout()
-    .command(["project", "--name", "jeff"])
-    .it("runs hello --name jeff", (ctx) => {
-      expect(ctx.stdout).to.contain("hello jeff");
+    .stub(prompts, "confirm", () => false)
+    .nock("https://api.progressively.app", (api) => {
+      api.get("/projects").reply(200, []);
+    })
+    .command(["project", "--create"])
+    .it("should skip project creation", (ctx) => {
+      expect(ctx.stdout).to.contain(
+        "You skipped project creation, you can still do this later or with web interface",
+      );
     });
 });
