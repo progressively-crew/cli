304f4e7a589e21706442ede9124ddad985ee8d47
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-08-13 17:26:20 +0200

    test: cover the register command

--- test/commands/register.test.ts
+++ test/commands/register.test.ts
@@ -1,17 +1,44 @@
 import { expect, test } from "@oclif/test";
+import * as prompts from "@inquirer/prompts";
+import sinon from "sinon";
+import * as configUtils from "../../src/utils/config";
+
+const configUtilsMock = sinon.mock(configUtils);
 
 describe("register", () => {
-  test
-    .stdout()
-    .command(["register"])
-    .it("runs hello", (ctx) => {
-      expect(ctx.stdout).to.contain("hello world");
-    });
+  configUtilsMock.expects("updateConfig").once();
 
   test
     .stdout()
-    .command(["register", "--name", "jeff"])
-    .it("runs hello --name jeff", (ctx) => {
-      expect(ctx.stdout).to.contain("hello jeff");
+    .stub(
+      prompts,
+      "input",
+      sinon
+        .stub()
+        .onCall(0)
+        .returns("Jean Smaug")
+        .onCall(1)
+        .returns("jean@smaug.fr"),
+    )
+    .stub(prompts, "password", () => "azertyuiop")
+    .nock("https://api.progressively.app", (api) => {
+      api.post("/auth/register").reply(201, {
+        fullname: "Jean Smaug",
+      });
+
+      api.post("/auth/login").reply(200, {
+        access_token: "MOCK_access_token",
+        refresh_token: "MOCK_refresh_token",
+      });
+
+      api.get("/auth/refresh").reply(200);
+    })
+    .command(["register"])
+    .it("should register an admin", (ctx) => {
+      configUtilsMock.verify();
+
+      expect(ctx.stdout).to.include(
+        "Jean Smaug, your account has been created 🎉",
+      );
     });
 });
