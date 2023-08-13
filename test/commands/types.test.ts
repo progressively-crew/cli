da351f8b9eb1e4d52f151ffa3218b338ea8d4247
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-08-13 14:19:39 +0200

    test: cover the type command

--- test/commands/types.test.ts
+++ test/commands/types.test.ts
@@ -1,17 +1,41 @@
 import { expect, test } from "@oclif/test";
+import sinon from "sinon";
+import fsPromises from "node:fs/promises";
+
+process.cwd = () => "/mock-current-folder";
+
+const fsPromisesMock = sinon.mock(fsPromises);
 
 describe("types", () => {
-  test
-    .stdout()
-    .command(["types"])
-    .it("runs hello", (ctx) => {
-      expect(ctx.stdout).to.contain("hello world");
-    });
+  fsPromisesMock
+    .expects("writeFile")
+    .withArgs(
+      "/mock-current-folder/progressively.d.ts",
+      'declare module "@progressively/types" {\n' +
+        "  export interface FlagDict {\n" +
+        "    background: boolean;\n" +
+        "  }\n" +
+        "  export interface FlagDictWithCustomString {\n" +
+        "    background: string | boolean;\n" +
+        "  }\n" +
+        "}\n",
+    )
+    .once();
 
   test
+    .nock("https://api.progressively.app", (api) =>
+      api
+        .get("/sdk/undefined/types/gen")
+        .reply(
+          200,
+          'declare module "@progressively/types" {export interface FlagDict {background: boolean;}export interface FlagDictWithCustomString {background: string | boolean;}}',
+        ),
+    )
     .stdout()
-    .command(["types", "--name", "jeff"])
-    .it("runs hello --name jeff", (ctx) => {
-      expect(ctx.stdout).to.contain("hello jeff");
+    .command(["types"])
+    .it("should generate a formatted type definition file", (ctx) => {
+      expect(ctx.stdout).to.contain("Your types file has been generated at");
+
+      fsPromisesMock.verify();
     });
 });
