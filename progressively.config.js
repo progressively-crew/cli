dc671df2879959114bce75fbc16fa1076ded56cb
jean-smaug <maximeblanc.dev@gmail.com>
2023-11-11 13:11:10 +0100

    install lilconfig which is used by prettier

---
+++ progressively.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  flags: [
+    {
+      name: "Home Page",
+      type: "boolean",
+      description: "Activate the new home page",
+    },
+    {
+      name: "Discount Banner",
+      type: "variant",
+      description: "Display the discount banner on the catalog",
+    },
+  ],
+};
