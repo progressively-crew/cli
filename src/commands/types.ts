e48dec5c386ebd0b91ca1c1bce8667b2148cc467
Marvin Frachet <marvin.frachet@protonmail.com>
2023-11-22 12:53:38 +0100

    fix(types): type generation with cascading questions (#46)

--- src/commands/types.ts
+++ src/commands/types.ts
@@ -1,5 +1,7 @@
-import { Command, Flags } from "@oclif/core";
+import { Command } from "@oclif/core";
+import fs from "node:fs";
 import fsPromises from "node:fs/promises";
+import path from "node:path";
 import prettier from "prettier";
 
 import { UserConfig, readUserConfig } from "../utils/config";
@@ -12,36 +14,35 @@ export default class Types extends Command {
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  static flags = {
-    name: Flags.string({
-      char: "N",
-      default: "progressively.d.ts",
-      description: "Name of the generated types file",
-      required: false,
-    }),
-    output: Flags.string({
-      char: "O",
-      default: process.cwd(),
-      description:
-        "Relative path of the folder where the types file should be generated",
-      required: false,
-    }),
-  };
+  static flags = {};
+
+  getTargetPath() {
+    const directory = path.join(
+      process.cwd(),
+      "node_modules",
+      "@types",
+      "@progressively",
+    );
+
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory, { recursive: true });
+    }
+
+    return path.join(directory, "index.d.ts");
+  }
 
   public async run(): Promise<void> {
-    const { flags } = await this.parse(Types);
+    const { client_key } = await this.guardConfig();
 
     const httpClient = await getHttpClient(true);
-    const { client_key } = await this.guardConfig();
 
     const { data } = await httpClient.get(`/sdk/${client_key}/types/gen`);
 
-    const targetFilePath = `${flags.output}/${flags.name}`;
-
     const formattedData = await prettier.format(data, { parser: "typescript" });
 
-    await fsPromises.writeFile(targetFilePath, formattedData);
+    const targetFilePath = this.getTargetPath();
 
+    await fsPromises.writeFile(targetFilePath, formattedData);
     this.log(`Your types file has been generated at ${targetFilePath}`);
   }
 
